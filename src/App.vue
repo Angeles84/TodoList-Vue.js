95ae5bc04814999e7f174a5cbd22b12f09001c8c
Angeles <angie.oliva@gmail.com>
2021-07-24 18:52:09 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <CrearTarea msg="Crear una nueva tarea"/>
+  <Formulario/>
+</template>
+
+<script>
+import CrearTarea from '@/components/CrearTarea.vue';
+import Formulario from '@/components/Formulario.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    CrearTarea,
+    Formulario
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #315f8d;
+  margin-top: 80px;
+}
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="formulario">
+    <label for="input">Tarea: </label>
+    <input id="input" type="text" placeholder="Ingresa una nueva tarea" v-model="nuevaLista">
+    <button @click="AgregarTarea">Crear</button>
+    <h3>Lista</h3>
+    <ul>
+      <li v-for="(lista, index) in agregar" :key="index"> 
+          {{ index }}: {{lista}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+ export default {
+    name: 'Formulario',
+    data: () => ({
+      nuevaLista: "",
+      agregar: []
+     
+    }),
+    methods: {
+      AgregarTarea() {
+        this.agregar.push(this.nuevaLista);
+        this.nuevaLista = ""; 
+      }
+    }
+  };
+</script>
+
+<style scoped>
+input, button {
+  padding: 5px;
+}
+input {
+  border: 1.7px solid #4b4b4b;
+}
+button {
+  background-color: #d6e2f5;
+  border: 1.7px solid #4b4b4b;
+  margin-left: 1px;
+  cursor: pointer;
+}
+ul {
+  width: 30%;
+  margin: 0 auto;
+  text-align: left;
+  padding-left: 16em;
+}
+</style>
